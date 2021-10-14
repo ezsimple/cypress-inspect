c13359ad180885406e83d440a63ba8ea39de426f
mhlee <mkeasy@gmail.com>
2021-10-14 17:01:34 +0900

    .

--- cypress/integration/장바구니.spec.js
+++ cypress/integration/장바구니.spec.js
@@ -2,7 +2,8 @@ import { get, login, post, prettyJSON, put } from '../libs/common';
 
 describe('장바구니', () => {
   before(() => {
-    login();
+    const req = { id: 'test@upleat.com', password: 'qwer1234' };
+    login(null, req);
   });
 
   // 장바구니
@@ -17,41 +18,41 @@ describe('장바구니', () => {
     });
   });
 
-  it('장바구니 저장', () => {
-    const q = 0;
-    const url = '/api/v1/orderCart';
-    const req = {
-      insertCartList: [
-        {
-          productCode: 'string',
-          artProductIndex: 0,
-          insertCartOptionList: [
-            {
-              optionMappingIndex: 0,
-            },
-          ],
-          quantity: 0,
-        },
-      ],
-    };
-    post(url, req).should((response) => {
-      expect(response.status).to.eq(201);
-      console.log(prettyJSON(response));
-    });
-  });
+  // it('장바구니 저장', () => {
+  //   const q = 0;
+  //   const url = '/api/v1/orderCart';
+  //   const req = {
+  //     insertCartList: [
+  //       {
+  //         productCode: 'string',
+  //         artProductIndex: 0,
+  //         insertCartOptionList: [
+  //           {
+  //             optionMappingIndex: 0,
+  //           },
+  //         ],
+  //         quantity: 0,
+  //       },
+  //     ],
+  //   };
+  //   post(url, req).should((response) => {
+  //     expect(response.status).to.eq(201);
+  //     console.log(prettyJSON(response));
+  //   });
+  // });
 
-  it('장바구니 수정', () => {
-    const q = 0;
-    const url = '/api/v1/orderCart';
-    const req = {};
-    put(url, req).should((response) => {
-      expect(response.status).to.eq(200);
-      console.log(prettyJSON(response));
-    });
-  });
+  // it('장바구니 수정', () => {
+  //   const q = 0;
+  //   const url = '/api/v1/orderCart';
+  //   const req = {};
+  //   put(url, req).should((response) => {
+  //     expect(response.status).to.eq(200);
+  //     console.log(prettyJSON(response));
+  //   });
+  // });
 
   it('장바구니 수정화면 상품 옵션 목록', () => {
-    const cartIndex = 7;
+    const cartIndex = 14;
     const url = '/api/v1/orderCart/options/' + cartIndex;
     const req = {};
     get(url, req).should((response) => {
