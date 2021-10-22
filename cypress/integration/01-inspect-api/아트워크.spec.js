c6551e1b27c493cfec4ce10e5f6feae4dd340cf2
mhlee <mkeasy@gmail.com>
2021-10-22 15:17:04 +0900

    .

--- cypress/integration/01-inspect-api/아트워크.spec.js
+++ cypress/integration/01-inspect-api/아트워크.spec.js
@@ -6,29 +6,32 @@ describe('아트워크', () => {
     login();
   });
 
-  it('아트워크 기본 정보', () => {
-    const artworkIndex = 55;
-    const url = host + '/api/v1/artwork/' + artworkIndex + '/info';
-    const req = {};
-    get(url, req).should((response) => {
-      expect(response.status).to.eq(200);
-      console.log(prettyJSON(response));
-    });
-  });
+  // 속도 이슈로 /artwork/summary로 통합
+  // it('아트워크 기본 정보', () => {
+  //   const artworkIndex = 55;
+  //   const url = host + '/api/v1/artwork/' + artworkIndex + '/info';
+  //   const req = {};
+  //   get(url, req).should((response) => {
+  //     expect(response.status).to.eq(200);
+  //     console.log(prettyJSON(response));
+  //   });
+  // });
 
-  it('아트워크 좋아요 카운터', () => {
-    const artworkIndex = 44;
-    const url = host + '/api/v1/artwork/' + artworkIndex + '/like/count';
-    const req = {};
-    get(url, req).should((response) => {
-      expect(response.status).to.eq(200);
-      console.log(prettyJSON(response));
-    });
-  });
+  // 속도 이슈로 /artwork/summary로 통합
+  // it('아트워크 좋아요 카운터', () => {
+  //   const artworkIndex = 44;
+  //   const url = host + '/api/v1/artwork/' + artworkIndex + '/like/count';
+  //   const req = {};
+  //   get(url, req).should((response) => {
+  //     expect(response.status).to.eq(200);
+  //     console.log(prettyJSON(response));
+  //   });
+  // });
 
   it('(셀럽그라운드 전용) 아트워크 데이터', () => {
     const artistMemberNo = 5;
-    const url = host + '/api/v1/celeb-ground/' + artistMemberNo + '/artwork/data';
+    const url =
+      host + '/api/v1/celeb-ground/' + artistMemberNo + '/artwork/data';
     const req = {};
     get(url, req).should((response) => {
       expect(response.status).to.eq(200);
