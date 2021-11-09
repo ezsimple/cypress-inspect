b5f442dd8e7173b7476d62d0c2047698f539fc9f
mhlee <mkeasy@gmail.com>
2021-11-09 15:26:56 +0900

    .

---
+++ cypress/integration/01-inspect-api/백오피스 - 정산.spec.js
@@ -0,0 +1,21 @@
+import { post, report } from '../../libs/common';
+
+describe('BO - 정산 (mhlee)', () => {
+  const host = Cypress.env('host-bo');
+  before(() => {
+    // adminLogin();
+  });
+
+  it('정산확정 내역', () => {
+    const url = host + '/bo/settleComplete/info';
+    const req = {
+      shopId: null,
+      memberId: 'test@upleat.com',
+      payAccountType: null,
+    };
+    post(url, req).should((response) => {
+      expect(response.status).to.eq(200);
+      report(url, response);
+    });
+  });
+});
